016ef2d8ad836dd5396fbad4a5f9ecb4c7e441af
Doug <doug.steinberg@gmail.com>
2017-05-22 14:07:40 -0400

    Added ability to move backward with 'B' command

--- rover.rb
+++ rover.rb
@@ -1,50 +1,33 @@
-# _ _ _ _ _ _ _ _ _
-# 9|_|_|_|_|_|_|_|_|_|        N
-# 8|_|_|_|_|_|_|_|_|_|      W   E
-# 7|_|_|_|_|_|_|_|_|_|        S
-# 6|_|_|_|_|_|_|_|_|_|
-# 5|_|_|_|_|_|_|_|_|_|
-# 4|_|_|_|_|_|_|_|_|_|
-# 3|_|_|_|_|_|_|_|_|_|
-# 2|_|_|_|_|_|_|_|_|_|
-# 1|_|_|_|_|_|_|_|_|_|
-# 0 1 2 3 4 5 6 7 8 9
-#
 # In order to control a rover, NASA sends a string of letters. The possible letters are 'L', 'R' and 'M'.
 #
 # 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
 # 'M' means move forward one grid point, and maintain the same heading.
-
-# Start by creating a Rover class that has these properties. Given that each rover starts with a location and direction, perhaps it's a
-# good idea to have an initialize method for each rover that accepts these parameters.
-# Next up we'll need to think about behaviour. Behaviours have the ability to read and affect the state of an object. We express those as methods.
-# read_instruction should accept an instruction and decide whether to tell the rover to move or turn. It simply delegates to more specific behaviour.
-# move affects the position of the rover. Depending on current direction and position, you'll need to update the x or y coordinates.
-# turn affects the direction of the rover. Depending on the current direction of the rover, the new direction will be determined with either turning L or R.
-# Once you've figured out how to move and turn your rover, the rest gets much easier because you got the seemingly complex stuff out of the
-# way first! All you'll need to figure out from here is how to read and split up the instructions so that you can pass them on to your rovers.
 
 class Rover
 
   attr_reader :x_position, :y_position, :direction
   attr_accessor :read_instruction, :turn_left, :turn_right
 
-  def initialize(x_position, y_position, direction)   #Starts witwh xy positions. Facing N/W/E/S
+  def initialize(x_position, y_position, direction)   #Starts with xy positions. Facing N/W/E/S
     @x_position = x_position
     @y_position = y_position
     @direction = direction
   end
 
-  def read_instruction(inputs) #should accept an instruction and decide whether to tell the rover to move or turn.
-    @input = inputs.split(//)
+  def read_instruction(inputs) #should accept an instruction and decide whether to tell the rover to move or turn
+    @input = inputs.downcase.split(//)
 
     @input.each do |instruction|  #iterate through every instruction (letter) in the input
       if instruction == "M"
         move  #run move method
+      elsif instruction == "B"
+        move_back #run move_back method
       elsif instruction == "L"
         turn_left  #run turn_left method
       elsif instruction == "R"
         turn_right  #run turn_right method
+      else
+        puts "Not a valid command. Please enter 'M', 'L', or 'R'"
       end
     end
 
@@ -86,6 +69,20 @@ class Rover
       @y_position -= 1
     elsif direction == "W"
       @x_position -= 1
+    end
+  end
+
+
+  def move_back
+    puts "Moving backward one space"
+    if direction == "N"
+      @y_position -= 1
+    elsif direction == "E"
+      @x_position -= 1
+    elsif direction == "S"
+      @y_position += 1
+    elsif direction == "W"
+      @x_position += 1
     end
   end
 
